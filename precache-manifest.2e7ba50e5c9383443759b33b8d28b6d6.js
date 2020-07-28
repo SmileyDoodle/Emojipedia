1a0a899c78b245266b6d6ce4332aaa91f12f438b
SmileyDoodle <smileydoodle.official@gmail.com>
2020-07-28 18:18:55 +0300

    Updates

---
+++ precache-manifest.2e7ba50e5c9383443759b33b8d28b6d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e83d54bec37463a3069b9d543893b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45ece57527eba84a64a6",
+    "url": "/static/js/2.cdbb3c3c.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.cdbb3c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d4837a54d869c37504",
+    "url": "/static/js/main.db54cdeb.chunk.js"
+  },
+  {
+    "revision": "13fb8c5fa137029ee659",
+    "url": "/static/js/runtime-main.83ecf5fb.js"
+  }
+]);
