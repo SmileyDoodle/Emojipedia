b0044bd594b1a0e90051405f8ba2a3ccc9c98db3
SmileyDoodle <smileydoodle.official@gmail.com>
2020-07-28 18:23:31 +0300

    Updates

---
+++ precache-manifest.e8bd10c8fd29104d1e9a7f8bbcbfaba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471bd4afdba6ce33b8f7dc03f0f675e8",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "45ece57527eba84a64a6",
+    "url": "/Emojipedia/static/js/2.cdbb3c3c.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Emojipedia/static/js/2.cdbb3c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d4837a54d869c37504",
+    "url": "/Emojipedia/static/js/main.db54cdeb.chunk.js"
+  },
+  {
+    "revision": "ebc739c0b131137c4617",
+    "url": "/Emojipedia/static/js/runtime-main.a18d6eed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e7ba50e5c9383443759b33b8d28b6d6.js"
+  "/Emojipedia/precache-manifest.e8bd10c8fd29104d1e9a7f8bbcbfaba1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Emojipedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
